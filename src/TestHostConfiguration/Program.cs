3a72cf906b7be2f8fd5dbaa891972476a6f28eb2
Jon Sagara <jon@sagarasoftware.com>
2023-08-31 08:39:41 -0700

    Initial commit.

---
+++ src/TestHostConfiguration/Program.cs
@@ -0,0 +1,22 @@
+﻿using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+
+try
+{
+    Log.Information("Hello, world.");
+
+    return 0;
+}
+catch (Exception ex)
+{
+    Log.Error(ex, "Unhandled exception in Program.Main.");
+
+    return -1;
+}
+finally
+{
+    Log.CloseAndFlush();
+}

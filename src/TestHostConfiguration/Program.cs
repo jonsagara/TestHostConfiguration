b721c9b078dc7ff82785189a979194c89876413b
Jon Sagara <jon@sagarasoftware.com>
2023-08-31 13:27:11 -0700

    Cleaned up code.

--- src/TestHostConfiguration/Program.cs
+++ src/TestHostConfiguration/Program.cs
@@ -1,8 +1,6 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Sagara.Core.Logging.Serilog;
+﻿using Microsoft.Extensions.Hosting;
 using Serilog;
-using TestHostConfiguration;
+using TestHostConfiguration.Hosting;
 
 Log.Logger = new LoggerConfiguration()
     .WriteTo.Console()
@@ -10,18 +8,9 @@ Log.Logger = new LoggerConfiguration()
 
 try
 {
-    //var hostAppBuilder = Host.CreateApplicationBuilder(args);
-    var configManager = new ConfigurationManager();
-    configManager.AddJsonFile("hostsettings.json", optional: true);
-    var hostAppBuilderSettings = new HostApplicationBuilderSettings
-    {
-        Args = args,
-        Configuration = configManager,
-    };
-    var hostAppBuilder = new HostApplicationBuilder(hostAppBuilderSettings);
-    hostAppBuilder.Logging.UseSerilog(configManager, ConfigureSerilog);
+    var builder = MyHost.CreateApplicationBuilder(args);
 
-    var host = hostAppBuilder.Build();
+    var host = builder.Build();
     host.Run();
 
     return 0;
@@ -35,28 +24,4 @@ catch (Exception ex)
 finally
 {
     Log.CloseAndFlush();
-}
-
-static void ConfigureSerilog(IConfiguration config, IServiceProvider services, LoggerConfiguration loggerConfig)
-{
-    // This is the .exe path in bin/{configuration}/{tfm}/
-    var logDir = Directory.GetCurrentDirectory();
-
-    // Log to the project directory.
-    logDir = Path.Combine(logDir, @"..\..\..");
-    logDir = Path.GetFullPath(logDir);
-    Log.Logger.Information("Logging directory: {logDir}", logDir);
-
-    // Serilog is our application logger. Default to Verbose. If we need to control this dynamically at some point
-    //   in the future, we can: https://nblumhardt.com/2014/10/dynamically-changing-the-serilog-level/
-
-    var logFilePathFormat = Path.Combine(logDir, "Logs", "log.txt");
-
-    // Always write to a rolling file.
-    loggerConfig
-        .ReadFrom.Configuration(config)
-        .ReadFrom.Services(services)
-        .Enrich.With<UtcTimestampEnricher>()
-        .WriteTo.Console()
-        .WriteTo.File(logFilePathFormat, outputTemplate: "{UtcTimestamp:yyyy-MM-dd HH:mm:ss.fff} [{Level}] [{SourceContext:l}] {Message}{NewLine}{Exception}", rollingInterval: RollingInterval.Day);
 }
---
+++ src/TestHostConfiguration/SerilogExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace TestHostConfiguration;
+
+public static class SerilogExtensions
+{
+    /// <summary>
+    /// Sets Serilog as the logging provider.
+    /// </summary>
+    /// <param name="builder">The logging builder to configure.</param>
+    /// <param name="config">The IConfiguration from which Serilog will attempt to read its configuration.</param>
+    /// <param name="configureLogger">The delegate for configuring the Serilog.LoggerConfiguration that will be used 
+    /// to construct a Serilog.Core.Logger.</param>
+    /// <param name="preserveStaticLogger">Indicates whether to preserve the value of Serilog.Log.Logger.</param>
+    /// <param name="writeToProviders">By default, Serilog does not write events to Microsoft.Extensions.Logging.ILoggerProviders 
+    /// registered through the Microsoft.Extensions.Logging API. Normally, equivalent Serilog sinks are used in place of providers. 
+    /// Specify true to write events to all providers.</param>
+    /// <returns>The logging builder.</returns>
+    public static ILoggingBuilder UseSerilog(this ILoggingBuilder builder, ConfigurationManager config, Action<IConfiguration, IServiceProvider, LoggerConfiguration> configureLogger, bool preserveStaticLogger = false, bool writeToProviders = false)
+    {
+        ArgumentNullException.ThrowIfNull(builder);
+        ArgumentNullException.ThrowIfNull(configureLogger);
+
+        IConfiguration config2 = config;
+        builder.Services.AddSerilog(
+            (serviceProvider, loggerConfiguration) =>
+                {
+                    configureLogger(config2, serviceProvider, loggerConfiguration);
+                },
+            preserveStaticLogger: preserveStaticLogger,
+            writeToProviders: writeToProviders
+            );
+
+        return builder;
+    }
+}
